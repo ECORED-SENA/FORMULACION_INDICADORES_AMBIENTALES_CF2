8898a5ec625671bd75ef8ffc09db1f4e17d74314
Andrea Botello <andrea.botellor@misena.edu.co>
2021-11-09 12:55:52 -0500

    updated

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
 .curso-main-container.pb-3
   BannerInterno
-  .container.tarjeta.tarjeta--blanca.p-4.p-md-5
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5.bloque-fondo-tema3
 
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero.text-white
@@ -91,8 +91,122 @@
 
     p.mb-4(data-aos="fade") Los indicadores ambientales se pueden clasificar según los datos disponibles. Estos son los tres tipos de indicadores ambientales que existen:
 
+    .row.justify-content-center.mb-5(data-aos="fade-down") 
+      .col-md-10
+        TabsA.color-acento-contenido
+          .tarjeta.bg-primario15.p-4(titulo="Tipo 1")
+            .row
+              .col-xl-6.mb-4
+                h4 Tipo 1
+                p Este tipo de indicador tiene datos que siempre se encuentran disponibles, ya que se obtienen en monitoreos que se realizan permanentemente. 
+              .col-xl-6
+               figure
+                  img(src='@/assets/curso/tema1-tab1.png', alt='Imagen decorativa')
+          .tarjeta.bg-primario15.p-4(titulo="Tipo 2")
+            .row
+              .col-xl-6.mb-4
+                h4 Tipo 2
+                p Estos están basados en los datos que provienen de monitoreo permanente, pero, a su vez, necesitan información adicional, dado que los datos pueden o no estar totalmente disponibles. 
+              .col-xl-6
+               figure
+                  img(src='@/assets/curso/tema1-tab2.png', alt='Imagen decorativa')    
+          .tarjeta.bg-primario15.p-4(titulo="Tipo 3")
+            .row
+              .col-xl-6.mb-4
+                h4 Tipo 3
+                p Este tipo de indicadores no tiene una base estadística, ya que son principalmente indicadores conceptuales. 
+              .col-xl-6
+               figure
+                  img(src='@/assets/curso/tema1-tab3.png', alt='Imagen decorativa')              
 
+    p.mb-4(data-aos="fade") A continuación, se presentan algunos ejemplos de indicadores ambientales. Es importante tener presente que en cada segmento pueden surgir nuevas propuestas de indicadores. 
 
+    .tarjeta.color-primario-degrade.text-white.py-5.mb-5
+      SlyderA(data-aos="fade-down")
+        .row.justify-content-center.mb-4
+          .col-md-5.mb-4.mb-md-0
+            h4 Índice de bienestar económico sostenible
+            p Este indicador está netamente relacionado con la economía y tiene como propósito reemplazar el Producto Interno Bruto (PIB) como el indicador de bienestar social añadiendo variables como el consumo ajustado y una medida de discrepancia socioeconómica.
+          .col-md-5
+            figure
+              img(src='@/assets/curso/tema1-slider1.png', alt='Índice de bienestar económico sostenible')
+        .row.justify-content-center.mb-4
+          .col-md-5.mb-4.mb-md-0
+            h4 Índice de desarrollo humano
+            p Este indicador tiene como finalidad determinar el estado de salud, de educación y de bienes económicos. 
+          .col-md-5
+            figure
+              img(src='@/assets/curso/tema1-slider2.png', alt='Índice de desarrollo humano') 
+        .row.justify-content-center.mb-4
+          .col-md-5.mb-4.mb-md-0
+            h4 Índice de sostenibilidad ambiental
+            p.mb-0 Es un instrumento utilizado para evaluar cuales son las formas de actividad humana que traen repercusiones directas al planeta. 
+            p Algunos de los factores que se tienen en cuenta son:
+            ul.lista-ul--color.acento-botones
+              li.mb-1
+                i.fas.fa-check
+                | Las emisiones de agentes contaminantes.
+              li.mb-1
+                i.fas.fa-check
+                | El uso de agroquímicos. 
+              li.mb-1
+                i.fas.fa-check
+                | La calidad y el volumen de aguas. 
+              li.mb-1
+                i.fas.fa-check
+                | El crecimiento de la población.
+              li.mb-1
+                i.fas.fa-check
+                | El consumo y la eficiencia energética.       
+          .col-md-5
+            figure
+              img(src='@/assets/curso/tema1-slider3.png', alt='Índice de sostenibilidad ambiental')           
+        .row.justify-content-center.mb-4
+          .col-md-5.mb-4.mb-md-0
+            h4 Índice de desempeño ambiental
+            p Este indicador se usa para evaluar el rendimiento ambiental de las políticas que algún país haya implementado en un periodo de tiempo determinado.  
+          .col-md-5
+            figure
+              img(src='@/assets/curso/tema1-slider4.png', alt='Índice de desempeño ambiental')
+        .row.justify-content-center.mb-4
+          .col-md-5.mb-4.mb-md-0
+            h4 Índice global de economía verde
+            p Este indicador analizó las inversiones que están realizando los países para tener una economía que sea sostenible con el medio ambiente.  
+          .col-md-5
+            figure
+              img(src='@/assets/curso/tema1-slider5.png', alt='Índice global de economía verde')
+        .row.justify-content-center.mb-4
+          .col-md-5.mb-4.mb-md-0
+            h4 Huella ecológica
+            p Este indicador evalúa el impacto ambiental que trae consigo la demanda de recursos naturales por parte del ser humano para satisfacer sus necesidades.  
+          .col-md-5
+            figure
+              img(src='@/assets/curso/tema1-slider6.png', alt='Huella ecológica')
+        .row.justify-content-center.mb-4
+          .col-md-5.mb-4.mb-md-0
+            h4 Huella de carbono
+            p Este indicador provee la información sobre la cantidad de gases de efecto invernadero que se produce de forma directa o indirecta. 
+          .col-md-5
+            figure
+              img(src='@/assets/curso/tema1-slider7.png', alt='Huella de carbono')
+        .row.justify-content-center.mb-4
+          .col-md-5.mb-4.mb-md-0
+            h4 Huella hídrica
+            p Este indicador es utilizado para cuantificar el uso del agua.  
+          .col-md-5
+            figure
+              img(src='@/assets/curso/tema1-slider8.png', alt='Huella hídrica')                        
+
+    
+    
+    p.mb-4(data-aos="fade") Los indicadores ambientales son una herramienta básica para analizar y realizar seguimiento a procesos de desarrollo, aunque las estrategias de desarrollo se apliquen a diferentes niveles de sociedad y sus consecuencias también se observen a diversas escalas.
+
+    p.mb-4(data-aos="fade") Es por esto que cada indicador debe buscar satisfacer las necesidades de cada uno de sus usuarios. En el siguiente recurso, se pueden observar los conceptos de indicadores y la interacción entre los mismos. 
+
+    .row.justify-content-center.mb-4(data-aos="fade-down") 
+      .col-md-10
+        .video
+          iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
   
 </template>
